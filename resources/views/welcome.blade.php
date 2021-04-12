e53d803e20432114f28538ef25188341f53e46ac
Afif Alauddin <afifalauddinomar@gmail.com>
2021-04-12 10:58:00 +0800

    Initial test

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+</head>
+
+<body>
+    <ol>
+        <li>
+            Please change this to login page.
+        </li>
+        <li>
+            Set middleware to the home controller.
+        </li>
+        <li>
+            Run php artisan migrate:refresh --seed (seed must contain 10 random user)
+        </li>
+        <li>
+            Fix Login.
+        </li>
+        <li>
+            Fix Registration.
+        </li>
+        <li>
+            Create a CRUD page of a user.
+        </li>
+    </ol>
+    <p></p>
+</body>
+
+</html>
